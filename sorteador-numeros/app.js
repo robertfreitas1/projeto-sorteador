6814e34e8543ab2fea20548144d8d3b753a7f36c
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-03-18 22:58:49 -0300

    commit

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,10 @@
+
+
+
+function sortear (){
+   let quantidade = document.getElementById ('quantidade').Value;
+   let de = document.getElementById ('de').Value;
+   let ate = document.getElementById ('ate').Value;
+
+    alert (`Quantidade: ${quantidade}`)
+}
