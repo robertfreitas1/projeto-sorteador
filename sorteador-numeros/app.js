962d7461a9fcb5dd807efd9be87241277f41a934
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-03-19 15:53:49 -0300

    usando loops for and while. também utilizando innerhtml para manipular o dom.

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,10 +1,30 @@
 
 
 
-function sortear (){
-   let quantidade = document.getElementById ('quantidade').Value;
-   let de = document.getElementById ('de').Value;
-   let ate = document.getElementById ('ate').Value;
+function sortear() {
+    let quantidade =  parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
-    alert (`Quantidade: ${quantidade}`)
+  let sorteados = [
+
+  ];
+ let numero;
+  for (let i = 0; i < quantidade; i++ ){
+    numero = obterNumeroAleatorio(de, ate);
+
+    while (sortedos.includes(numero)){
+        numero =  obterNumeroAleatorio(de, ate);
+    }
+
+    sorteados.push(numero);
+  }
+
+  let resultado =document.getElementById('resultado')
+  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`
 }
+
+    function obterNumeroAleatorio(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
